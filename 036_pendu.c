d624b31ff9e1fc99c0df9ceec01160f8fa3d64f2
Julien Coignet <breddabasse@hotmail.com>
2017-01-23 17:40:18 +0100

    Jeu du Pendu, version perso

--- 036_pendu.c
+++ 036_pendu.c
@@ -7,21 +7,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void display_word(char* word, int* tab_tried, int* tab_found)
+#define CHAR_FOUND 				1
+#define CHAR_ALREADY_TRIED		2
+#define CHAR_NOT_FOUND			3
+#define CHAR_FIRST_TRY			4
+
+int display_word(char* word, int* tab_tried);
+char upper_char(char c);
+void upper_word(char* word);
+int try_char(char c, int* tab_tried, char* word);
+void array_debug(int* tab, int len);
+int already_tried(char c, int* tab_tried);
+
+
+int display_word(char* word, int* tab_tried)
 {
 	int i = 0;
-	char* word_to_display = "";
+	int nbletterstofind = 0;
 
 	while (word[i] != '\0')
-	{
+	{	
+		if (word[i] >= 65 && word[i] <= 90)
+		{
+			if (tab_tried[word[i] - 65] == 1)
+				printf("%c", word[i]);
+			else
+			{
+				printf("_");
+				nbletterstofind++;
+			}
+		}
+		else 
+			printf("%c", word[i]);
 
 		i++;
 	}
+
+	return nbletterstofind;
 }
 
 char upper_char(char c)
 {
-	if ((c >= 97) && (c <= 123))
+	if ((c >= 97) && (c <= 122))
 		return (c - 32);
 	else
 		return (c);
@@ -38,9 +65,27 @@ void upper_word(char* word)
 	}
 }
 
-int try_char(char c, int* tab_tried, int* tab_found, char* word)
+int try_char(char c, int* tab_tried, char* word)
 {
+	if (already_tried(c, tab_tried))
+		return CHAR_ALREADY_TRIED;
 
+	tab_tried[c - 65] = 1;
+
+	int i;
+	while (word[i] != '\0')
+	{
+		if (word[i] == c)
+			return CHAR_FOUND;
+		i++;
+	}
+
+	return CHAR_NOT_FOUND;
+}
+
+int already_tried(char c, int* tab_tried)
+{
+	return tab_tried[c - 65];
 }
 
 void array_debug(int* tab, int len)
@@ -61,53 +106,69 @@ void array_debug(int* tab, int len)
 
 int main(int argc, char* argv[])
 {
-	char* word = "default";
+	char* word;
 	int nblives = 7;
 
 	int tab_tried[26];
-	int tab_found[26];
+
+	char typedletter;
+
+	int letterstofind;
 
 	int i;
 
 	for (i=0; i<26; i++)
-	{
 		tab_tried[i] = 0;
-		tab_found[i] = 0;
-	}
 
-	if (argc == 1)
-		upper_word(word);
-	else if (argc == 2)
-		word = argv[1];
-	else if (argc == 3)
+	if (argc >= 2)
 	{
 		word = argv[1];
 		upper_word(word);
-		nblives = *argv[2] - '0'; // Conversion du caractère en entier
 	}
+	else
+		word = "MACHINBIDULE";
 
-	tab_tried[word[0] - 'A'] = 1;
-	tab_found[word[0] - 'A'] = 1;
+	if (argc >= 3)
+		nblives = atoi(argv[2]);
+	else
+		nblives = 7;		
 
-	printf("%s\n", word);
-	printf("%d\n", nblives);
+	printf("Jeu du Pendu. Vous avez droit à %d essais.\n", nblives);
 
-	int found = 0;
+	try_char(word[0], tab_tried, word);
 
-	/*while ((found == 0) && (nblives > 0))
-	{
-
-	}*/
-
-	printf("%d\n", 'a');
-	printf("%d\n", 'A');
-
-	printf("%c\n", upper_char('A'));
-	printf("%c\n", upper_char('a'));
-	printf("%c\n", upper_char('.'));
-
-	array_debug(tab_tried, 26);
+	letterstofind = display_word(word, tab_tried);
 	printf("\n");
+
+	while ((nblives > 0) && (letterstofind > 0))
+	{	
+		int res;
+
+		printf("Saisissez une lettre : ");
+		scanf("%c", &typedletter);
+
+		// On vide le buffer, sinon ça déconne
+		char c;
+		while ((c = getchar()) != '\n' && c != EOF) { }
+
+		res = try_char(upper_char(typedletter), tab_tried, word);
+		if (res == CHAR_ALREADY_TRIED)
+			printf("La lettre %c a déjà été placée, ou testée.\nIl vous reste toujours %d essai(s).\n", upper_char(typedletter), nblives);
+		if (res == CHAR_NOT_FOUND)
+		{
+			nblives--;
+			printf("La lettre %c n'est pas dans le mot.\nIl vous reste %d essai(s).\n", upper_char(typedletter), nblives);
+		}
+
+		letterstofind = display_word(word, tab_tried);
+		printf("\n");
+	}
+
+	if (nblives <= 0)
+		printf("\nNombre de tentatives maximum atteint.\nVous avez perdu...\n");
+
+	if (letterstofind <= 0)
+		printf("\nLe mot à trouver était : %s.\nVous avez gagné !\n", word);
 
 	return EXIT_SUCCESS;
 }
